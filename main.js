355a25b9e19bdcb28ddfee40930cee1d456ae614
Taylor <tbnelson01@gmail.com>
2022-09-28 13:52:55 -0400

    Mytunes search

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-const url = 'https://itunes.apple.com/search?term=migos&limit=20'
-let searchResults = document.querySelector("#search-results")
 
-fetch(url)
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (musics) {
-        buildResults(musics.results);
-    })
+let input = document.getElementById("music-search")
+let form = document.getElementById("form")
+let searchResults = document.getElementById("search-results")
+let audio = document.getElementById("audio-player")
+
+form.addEventListener("submit", (event) => {
+    let search = input.value;
+    let url = `https://itunes.apple.com/search?term=${search}&limit=20`
+    event.preventDefault();
+
+    fetch(url)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (music) {
+            searchResults.innerHTML = '';
+            buildResults(music.results);
+        })
+})
 
 function buildResults(itunesArray) {
     for (let itunes of itunesArray) {
@@ -15,24 +25,37 @@ function buildResults(itunesArray) {
         let picture = document.createElement('img')
         let title = document.createElement('h3')
         let artist = document.createElement('h4')
-        let audio = document.getElementById("audio-player")
-        resultsDiv.classList.add("results");
+        resultsDiv.classList.add("resultsBorder", "results");
+        title.classList.add('songTitle')
 
-        audio.src = `${itunes.previewUrl}`
         picture.src = `${itunes.artworkUrl100}`
         title.innerText = `${itunes.trackName}`
         artist.innerText = `${itunes.artistName}`
 
+        resultsDiv.addEventListener("click", (event) => {
+            audio.src = ''
+            audio.src = `${itunes.previewUrl}`
+            audio.volume = 0.1;
+        })
+
         resultsDiv.appendChild(picture);
         resultsDiv.appendChild(title);
         resultsDiv.appendChild(artist);
-        searchResults.appendChild(resultsDiv);
+        searchResults.appendChild(resultsDiv)
 
-
-        resultsDiv.addEventListener("click", (event) => {
-            audio.play();
+        resultsDiv.addEventListener("mouseover", (event) =>{
+            resultsDiv.classList.add('hover')
         })
-
+        resultsDiv.addEventListener("mouseout", (event) =>{
+            resultsDiv.classList.remove('hover')
+        })
+        resultsDiv.addEventListener("mousedown", (event) =>{
+            resultsDiv.classList.remove('resultsBorder')
+        })
+        resultsDiv.addEventListener("mouseup", (event) =>{
+            resultsDiv.classList.add('resultsBorder')
+        })
+    
+    
     }
 }
-
