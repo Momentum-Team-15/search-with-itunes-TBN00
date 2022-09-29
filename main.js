84f20c6e2225ac69fbc56e52bbfd0280921dffdc
Taylor <tbnelson01@gmail.com>
2022-09-29 11:53:52 -0400

    radio button progress

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
-
+//Declaring variables for user input in the form
 let input = document.getElementById("music-search")
 let form = document.getElementById("form")
+//Declaring variables to hold search results and link to audio
 let searchResults = document.getElementById("search-results")
 let audio = document.getElementById("audio-player")
 
+
+let radioButtons = document.querySelectorAll('input[name="radioB"]');
+
+//Adding event listener to form to gather user input
 form.addEventListener("submit", (event) => {
     let search = input.value;
-    let url = `https://itunes.apple.com/search?term=${search}&limit=20`
+    let url = `https://itunes.apple.com/search?term=${search}`
+    for (let radioButton of radioButtons){
+        if (radioButton.checked){
+            url+= `&entity=${radioButton.value}`
+        }
+    }
+
+
     event.preventDefault();
 
+    //Using fetch function and user input to gather info from iTunes API
     fetch(url)
         .then(function (response) {
             return response.json()
         })
         .then(function (music) {
+            //clearing search results before calling function
             searchResults.innerText = '';
+            //calling buildResults function to send search results to page
             buildResults(music.results);
         })
 })
 
+//creation of function buildResults
 function buildResults(itunesArray) {
     for (let itunes of itunesArray) {
         let resultsDiv = document.createElement('div')
@@ -26,36 +42,39 @@ function buildResults(itunesArray) {
         let title = document.createElement('h3')
         let artist = document.createElement('h4')
         resultsDiv.classList.add("resultsBorder", "results");
-        title.classList.add('songTitle')
+
 
         picture.src = `${itunes.artworkUrl100}`
         title.innerText = `${itunes.trackName}`
         artist.innerText = `${itunes.artistName}`
 
+        //Creating event listener on results to play song preview on click
         resultsDiv.addEventListener("click", (event) => {
             audio.src = ''
             audio.src = `${itunes.previewUrl}`
             audio.volume = 0.1;
         })
 
+        //Appending results to show picture, song title, and artist
         resultsDiv.appendChild(picture);
         resultsDiv.appendChild(title);
         resultsDiv.appendChild(artist);
         searchResults.appendChild(resultsDiv)
 
-        resultsDiv.addEventListener("mouseover", (event) =>{
+        //Adding event listeners on results to highlight songs
+        resultsDiv.addEventListener("mouseover", (event) => {
             resultsDiv.classList.add('hover')
         })
-        resultsDiv.addEventListener("mouseout", (event) =>{
+        resultsDiv.addEventListener("mouseout", (event) => {
             resultsDiv.classList.remove('hover')
         })
-        resultsDiv.addEventListener("mousedown", (event) =>{
+        resultsDiv.addEventListener("mousedown", (event) => {
             resultsDiv.classList.remove('resultsBorder')
         })
-        resultsDiv.addEventListener("mouseup", (event) =>{
+        resultsDiv.addEventListener("mouseup", (event) => {
             resultsDiv.classList.add('resultsBorder')
         })
-    
-    
+
+
     }
 }
